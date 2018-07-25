ecff308fb6304a31db46d7523f554cc54be4eaad
Gromtsev Mikhail <m.gromtsev@gmail.com>
2018-07-25 14:35:35 +0600

    Add base moxy mwp Activity and Fragment classes

---
+++ app/src/main/java/com/example/michailgromtsev/startandroidvk/ui/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.michailgromtsev.startandroidvk.ui.activity;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.example.michailgromtsev.startandroidvk.ui.fragment.BaseFragment;
+
+public class BaseActivity extends MvpAppCompatActivity {
+
+    //method change toolbar name and visibly of floating button
+    public void fragmentOnScreen(BaseFragment fragment){
+
+    }
+}
---
+++ app/src/main/java/com/example/michailgromtsev/startandroidvk/ui/fragment/BaseFragment.java
@@ -0,0 +1,6 @@
+package com.example.michailgromtsev.startandroidvk.ui.fragment;
+
+import com.arellomobile.mvp.MvpAppCompatFragment;
+
+public class BaseFragment extends MvpAppCompatFragment {
+}
